601ded3101e7e60d91c96622722598776f2852e4
1sh4sk <ayeshask.24a.mailme@gmail.com>
2024-08-20 18:20:27 +0530

    profile img

--- src/translations/english/common.js
+++ src/translations/english/common.js
@@ -1,3 +1,5 @@
+import Profile from '../../assets/profile.jpg'
+
 export default {
     hero: {
         heading: "Elevate Your Clean Game with Our Trusted Products",
@@ -64,19 +66,19 @@ export default {
         heading: "Testimonials",
         testimonialcontent: [
             {
-                img: '/src/assets/profile.jpg',
+                img: Profile,
                 content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
             },
             {
-                img: '/src/assets/profile.jpg',
+                img: Profile,
                 content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
             },
             {
-                img: '/src/assets/profile.jpg',
+                img: Profile,
                 content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
             },
             {
-                img: '/src/assets/profile.jpg',
+                img: Profile,
                 content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
             },
         ]
